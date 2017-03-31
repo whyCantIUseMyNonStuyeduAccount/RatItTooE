7e5b54cd5abbe0611a0182557bd8c7e63a6b01ec
JonathanQuang <jquang@stuy.edu>
2017-03-31 07:41:36 -0400

    java files empty shells and stuff

--- DD.java
+++ DD.java
@@ -1,4 +1,19 @@
-public class DD implements Deque {
+public class DD<T> implements Deque<T> {
+
+    private LLNode front, end;
+    private int size;
+ 
+    public DD() {
+	front = end = null;
+    }
+    
+    public void addFirst(T val){
+	LLN
+	if (size==0){
+	    front
+	}
+    } 
+
 
 
 
