f7d43c7bba33c8428be0f473285c45db8f3fc51c
JonathanQuang <jquang@stuy.edu>
2017-04-03 07:05:25 -0400

    uncommented stuff in interface

--- Deque.java
+++ Deque.java
@@ -1,17 +1,17 @@
 public interface Deque<T> {
 
     public void addFirst(T e);
-    //public void addLast(T e);
+    public void addLast(T e);
 
-    //public boolean contains(Object O);
+    public boolean contains(Object O);
 
-    //public T getFirst();
-    //public T getLast();
+    public T getFirst();
+    public T getLast();
 
-    //public T removeFirst(Object O);
-    //public T removeLast(Object O);
+    public T removeFirst(Object O);
+    public T removeLast(Object O);
 
-    //public int size();
+    public int size();
 
     
 }
