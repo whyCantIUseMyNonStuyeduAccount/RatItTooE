773461e9714c2013dca0b8d597e96331faccd882
Edward Ro <ero@stuy.edu>
2017-04-03 08:33:09 -0400

    Added commented out methods add(), removeFirst and Last Occurences()

--- Deque.java
+++ Deque.java
@@ -1,5 +1,7 @@
 public interface Deque<T> {
-
+    
+    //public boolean add( T e ); 
+    //Didn't add because the next two functions under it will make it redundant.
     public void addFirst( T e );
     public void addLast( T e );
 
@@ -7,7 +9,11 @@ public interface Deque<T> {
 
     public T getFirst();
     public T getLast();
-
+    
+    //public boolean removeFirstOccurence( Object O );
+    //public boolean removeLastOccurence( Object O );
+    //Didn't add because deque shouldn't be able to edit the middle of the deque
+    
     public T removeFirst();
     public T removeLast();
 
